7b29ec52ffc03e452d93d2115915fc2fbd0a4807
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-09-01 23:24:42 +0300

    sad

--- client/src/components/Changelog.tsx
+++ client/src/components/Changelog.tsx
@@ -24,22 +24,20 @@ const Changelog: React.FC = () => {
 
   const { loading, error, data } = useQuery<{ changeLogs: ChangeLog[] }>(GET_CHANGELOGS);
 
-
   if (loading) {
     if (error) {
       console.error('Error fetching changelogs:', error);
     }
-   
+
     return (
       <div className="w-full min-h-screen flex flex-col items-center justify-center">
         <Spinner w={10} h={10}/>
-        {(error) && (
+        {error && (
            <Message width='w-fit' title="Attention Needed" padding="p-5" titleMarginBottom="mb-2" message="Something went wrong. Please try again later." type="error" />
         )}
       </div>
     );
   }
-
 
   const changelogData: FormattedChangeLog[] = data?.changeLogs?.map((log: ChangeLog): FormattedChangeLog => {
     const date = new Date(log.createdAt);
@@ -66,24 +64,28 @@ const Changelog: React.FC = () => {
           Latest updates and improvements to the app.
         </p>
 
-        <ul className="space-y-6">
-          {changelogData.map((log, index) => (
-            <li key={index} className="p-6 bg-white dark:bg-box rounded-lg shadow">
-              <div className="flex justify-between items-center">
-                <h2 className="text-md font-semibold text-gray-800 dark:text-white">Version {log.version}</h2>
-                <span className="text-gray-600 dark:text-gray-400 text-sm">{log.date}</span>
-              </div>
-              <ul className="mt-3 space-y-2 text-gray-700 dark:text-gray-300">
-                {log.changes.map((change, idx) => (
-                  <li key={idx} className="flex items-start text-sm">
-                    <span className="mr-2 text-green-500">•</span>
-                    <p>{change}</p>
-                  </li>
-                ))}
-              </ul>
-            </li>
-          ))}
-        </ul>
+        {changelogData.length === 0 ? (
+          <Message width='w-full' title="No Changelogs Available" padding="p-5" titleMarginBottom="mb-2" message="There are no changelogs available at the moment." type="info" />
+        ) : (
+          <ul className="space-y-6">
+            {changelogData.map((log, index) => (
+              <li key={index} className="p-6 bg-white dark:bg-box rounded-lg shadow">
+                <div className="flex justify-between items-center">
+                  <h2 className="text-md font-semibold text-gray-800 dark:text-white">Version {log.version}</h2>
+                  <span className="text-gray-600 dark:text-gray-400 text-sm">{log.date}</span>
+                </div>
+                <ul className="mt-3 space-y-2 text-gray-700 dark:text-gray-300">
+                  {log.changes.map((change, idx) => (
+                    <li key={idx} className="flex items-start text-sm">
+                      <span className="mr-2 text-green-500">•</span>
+                      <p>{change}</p>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );

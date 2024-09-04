8827e2fa36a41875021ef226118ede3a64d8a9e9
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-09-04 11:23:15 +0300

    asd

--- client/src/components/BagDetails.tsx
+++ client/src/components/BagDetails.tsx
@@ -181,7 +181,7 @@ const BagDetails: React.FC = () => {
                     </h1>
                   </div>
                 </div>
-                <div className='w-full sm:w-4/12 flex flex-row items-center justify-end'>
+                <div className='w-fit sm:w-4/12 flex flex-row items-center justify-end'>
                   <label className="inline-flex items-center cursor-pointer mr-5 hidden sm:block">
                     <input type="checkbox" checked={bag?.exploreBags} onChange={handleToggleShareToExplore} className="sr-only peer" />
                     <div className="relative w-11 h-6 bg-gray-300 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-600 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-400"></div>

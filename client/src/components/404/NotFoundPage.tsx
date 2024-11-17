6db7cf900c7d00a7f2dc556af8706d9d2e7f67e2
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-11-17 19:18:23 +0200

    changes

--- client/src/components/404/NotFoundPage.tsx
+++ client/src/components/404/NotFoundPage.tsx
@@ -7,8 +7,8 @@ const NotFoundPage: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen dark:bg-zinc-900 bg-gray-100">
       <div className="text-center">
-        <h1 className="text-9xl font-extrabold text-orange-400 dark:text-orange-300 flex flex-row items-center justify-center">
-          4<img src="/images/favicon.ico" alt="Logo" className="w-24 h-24 mx-2" />4
+        <h1 className="text-9xl font-extrabold text-primary flex flex-row items-center justify-center">
+          404
         </h1>
         <p className="text-2xl text-gray-800 dark:text-white mt-4">Oops! Page Not Found</p>
         <p className="text-md text-gray-600 dark:text-gray-300 mt-2">
@@ -17,9 +17,9 @@ const NotFoundPage: React.FC = () => {
         <div className="w-full mt-6 flex justify-center">
           <button
             onClick={() => navigate('/')}
-            className="px-7 py-3 font-semibold text-sm text-white bg-blue-500 rounded hover:bg-blue-600"
+            className="px-7 py-2 text-sm text-white bg-accent rounded hover:bg-gray-400"
           >
-            Go to Homepage
+            Home page
           </button>
         </div>
       </div>

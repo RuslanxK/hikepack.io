16deb2ef0cd9166bdf8f6e33e6806a09ba34bb55
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-12-02 22:11:24 +0200

    paypal payment added

--- client/src/components/popups/Modal.tsx
+++ client/src/components/popups/Modal.tsx
@@ -3,26 +3,28 @@ import { createPortal } from 'react-dom';
 import { ModalProps } from '../../types/modal';
 import { MdCancel } from "react-icons/md";
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children }) => {
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, title, children, customClassName }) => {
   if (!isOpen) return null;
 
   return createPortal(
     <div className="fixed inset-0 p-2 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="flex items-center justify-center w-full">
-        <div className="bg-white dark:bg-theme-dark p-8 rounded-lg shadow-airbnb w-full h-full sm:max-w-lg sm:max-h-full relative">
+      <div className={`flex items-center justify-center w-full`}>
+        <div
+          className={`bg-white dark:bg-theme-dark p-8 rounded-lg shadow-airbnb w-full h-full sm:max-h-full relative ${
+            customClassName || 'sm:max-w-lg'
+          }`}
+        >
           <div className="flex justify-between items-center">
             <h2 className="text-xl font-semibold text-black dark:text-white mb-4">{title}</h2>
             <button
               className="text-black dark:text-white hover:text-accent dark:hover:text-accent"
               onClick={onClose}
             >
-               <MdCancel size={20} className='mb-5'/>
+              <MdCancel size={20} className="mb-5" />
             </button>
-           
           </div>
-          <div className=" h-full box-border">
-            {children}
-          </div>
+          <div className="h-full box-border">{children}</div>
         </div>
       </div>
     </div>,
--- client/src/components/popups/SupportUsModel.tsx
+++ client/src/components/popups/SupportUsModel.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
-import Modal from './Modal';
+import React, { useState } from "react";
+import Modal from "./Modal";
 import { BiSolidCoffeeAlt } from "react-icons/bi";
-
+import {
+  PayPalScriptProvider,
+  PayPalButtons,
+  ReactPayPalScriptOptions,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
+import { MdClose } from "react-icons/md";
+import Spinner from "../loading/Spinner";
 
 interface SupportUsModalProps {
   isOpen: boolean;
@@ -10,75 +17,132 @@ interface SupportUsModalProps {
 
 const SupportUsModal: React.FC<SupportUsModalProps> = ({ isOpen, onClose }) => {
   const [selectedCoffee, setSelectedCoffee] = useState<number>(1);
-  const [coffeeOptions, setCoffeeOptions] = useState<number[]>([1, 3, 5, 10]);
-  const [customCoffeeInput, setCustomCoffeeInput] = useState<string>('');
-  const pricePerCoffee = 5;
+  const coffeeOptions = [1, 3, 5, 10];
+  const pricePerCoffee = 1;
 
-  const handleAddCustomCoffee = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      const customValue = parseInt(customCoffeeInput, 10);
-      if (!isNaN(customValue) && customValue > 0 && !coffeeOptions.includes(customValue)) {
-        setCoffeeOptions([...coffeeOptions, customValue]); 
-        setSelectedCoffee(customValue); 
-        setCustomCoffeeInput(''); 
-      }
-    }
+  const paypalOptions: ReactPayPalScriptOptions = {
+    clientId: process.env.REACT_APP_PAYPAL_CLIENT || "",
+    currency: "USD",
   };
 
-  const handleSupport = () => {
-    alert(`You chose to buy ${selectedCoffee} coffee(s) for $${selectedCoffee * pricePerCoffee}.`);
+  const PayPalButtonsWrapper: React.FC = () => {
+    const [{ isPending }] = usePayPalScriptReducer();
+
+    return isPending ? (
+      <div className="flex justify-center items-center h-32">
+        <Spinner w={4} h={4} />
+      </div>
+    ) : (
+      <PayPalButtons
+        style={{
+          shape: "rect",
+          layout: "vertical",
+          color: "gold",
+          label: "pay",
+        }}
+        createOrder={(data, actions) => {
+          return actions.order.create({
+            intent: "CAPTURE",
+            purchase_units: [
+              {
+                amount: {
+                  currency_code: "USD",
+                  value: (selectedCoffee * pricePerCoffee).toFixed(2),
+                },
+              },
+            ],
+          });
+        }}
+        onApprove={(data, actions) => {
+          if (actions.order) {
+            return actions.order.capture().then((details) => {
+              const payerName = details?.payer?.name?.given_name || "Customer";
+              alert(`Transaction completed by ${payerName}`);
+              onClose();
+            });
+          }
+          return Promise.reject("Order action is not available");
+        }}
+        onError={(err) => {
+          alert(`Something went wrong: ${err}`);
+        }}
+      />
+    );
   };
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} title="Who We Are">
-      <div className="flex flex-col w-full gap-6">
-        <div className="w-full">
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Buy Us a Coffee"
+      customClassName="sm:max-w-4xl"
+    >
+      <div
+        className="flex flex-col items-start justify-between md:flex-row w-full gap-6"
+        style={{
+          maxHeight: "80vh",
+          overflowY: "auto",
+        }}
+      >
+        {/* Left Content Section */}
+        <div className="md:w-6/12 sticky top-0">
           <p className="text-sm text-gray-700 dark:text-gray-300 mb-4">
-          We are a team of developers and designers who love hiking and actively enjoy the outdoors. Our mission is to help hikers organize their trip & bags with ease and share their journeys with others.
+            We are a team of developers and designers who love hiking and actively enjoy the outdoors. Our mission is to help hikers organize their trips & bags with ease and share their journeys with others.
           </p>
           <p className="text-sm text-gray-700 dark:text-gray-300">
-          If our work has made a positive impact on your hiking experience and you’d like to show your support, feel free to buy us a coffee!
+            If our work has made a positive impact on your hiking experience and you’d like to show your support, feel free to buy us a coffee!
           </p>
+          <p className="text-sm text-gray-800 dark:text-gray-200 mb-1 mt-4 font-semibold">Choose your coffee amount:</p>
+
+          <div className="mt-4 bg-gray-100 p-4 border rounded-lg">
+            <div className="flex flex-wrap gap-3 items-center">
+              <BiSolidCoffeeAlt size={30} className="text-primary" />
+              <MdClose size={25} className="text-accent" />
+              {coffeeOptions.map((option) => (
+                <button
+                  key={option}
+                  className={`px-5 py-1.5 rounded-lg border ${
+                    selectedCoffee === option
+                      ? "bg-primary text-white"
+                      : "bg-white dark:bg-black text-gray-800 dark:text-gray-200 dark:border-accent"
+                  } hover:bg-primary hover:text-white transition`}
+                  onClick={() => setSelectedCoffee(option)}
+                >
+                  {option}
+                </button>
+              ))}
+
+              <input
+                type="number"
+                min="1"
+                value={selectedCoffee}
+                placeholder="   +"
+                className="w-12 py-1.5 border text-md rounded-lg text-gray-800 dark:text-gray-200 dark:bg-black dark:border-accent focus:outline-none focus:ring focus:ring-primary text-center placeholder:text-gray-400"
+                onChange={(e) => {
+                  const value = parseInt(e.target.value, 10);
+                  setSelectedCoffee(!isNaN(value) && value > 0 ? value : 1);
+                }}
+              />
+            </div>
+          </div>
+          <div className="text-sm text-gray-800 dark:text-gray-200 mt-5">
+            <p>
+              You are about to pay:{" "}
+              <span className="font-semibold text-primary">
+                ${(selectedCoffee * pricePerCoffee).toFixed(2)}
+              </span>
+            </p>
+          </div>
         </div>
 
-        <div className="w-full bg-gray-100 dark:bg-black p-4 rounded-lg border dark:border-accent">
-          <div className='flex flex-row justify-between w-full items-center'>
-          <h3 className="text-lg font-semibold mb-3 dark:text-white">Buy Us a Coffee</h3>
-          <BiSolidCoffeeAlt size={30} className="dark:text-white" />
+        {/* Right Payment Section */}
+        <div className="sm:w-5/12 w-full bg-gray-100 dark:bg-black p-4 rounded-lg border dark:border-accent">
+          <div className="flex flex-row justify-between w-full items-center mb-4">
+            <h3 className="text-lg font-semibold dark:text-white">Payment</h3>
           </div>
-          <p className="text-sm text-gray-800 dark:text-gray-200 mb-4">Choose your coffee amount:</p>
-          <input
-              type="number"
-              min="1"
-              value={customCoffeeInput}
-              placeholder="Add x"
-              className="w-24 px-3 py-2 border rounded-lg text-gray-800 dark:text-gray-200 dark:bg-black dark:border-accent focus:outline-none focus:ring focus:ring-primary"
-              onChange={(e) => setCustomCoffeeInput(e.target.value)}
-              onKeyDown={handleAddCustomCoffee}
-            />
-
-          <div className="flex flex-wrap gap-3 mb-6 mt-6 items-center">
-            {coffeeOptions.map((option) => (
-              <button
-                key={option}
-                className={`px-5 py-2 rounded-lg border ${
-                  selectedCoffee === option
-                    ? 'bg-primary text-white'
-                    : 'bg-white dark:bg-black text-gray-800 dark:text-gray-200 dark:border-accent'
-                } hover:bg-primary hover:text-white transition`}
-                onClick={() => setSelectedCoffee(option)}
-              >
-                {option}x
-              </button>
-            ))}
-           
-          </div>
-          <button
-            onClick={handleSupport}
-            className="w-full bg-primary hover:bg-button-hover text-white py-2 rounded-lg hover:bg-primary-dark transition"
-          >
-            Support ${selectedCoffee * pricePerCoffee}
-          </button>
+          <PayPalScriptProvider options={paypalOptions}>
+            <PayPalButtonsWrapper />
+          </PayPalScriptProvider>
         </div>
       </div>
     </Modal>

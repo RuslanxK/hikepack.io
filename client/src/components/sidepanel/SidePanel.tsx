b19a9355e04e52ac5fc1c40a4da0ef1aa15eef96
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-12-05 22:21:56 +0200

    changes

--- client/src/components/sidepanel/SidePanel.tsx
+++ client/src/components/sidepanel/SidePanel.tsx
@@ -17,17 +17,17 @@ const SidePanel: React.FC<SidePanelProps> = ({ isVisible, toggleVisibility, item
   
   return (
     <div
-      className={`bg-primary sm:dark:bg-box dark:bg-theme-bgDark w-1/2 sm:w-56 h-screen fixed right-0 top-0 transition-transform z-50  ${
+      className={`bg-primary sm:dark:bg-box dark:bg-theme-bgDark w-1/2 sm:w-56 h-screen fixed right-0 top-0 transition-transform z-40  ${
         isVisible ? 'translate-x-0' : 'translate-x-full'
       }`}
     >
       <button
         onClick={toggleVisibility}
-        className={`fixed sm:top-0 ${isVisible ? "top-0" : "top-20"}  ${isVisible ? "left-0" : "left-[-25px]"} rounded bg-button-orange dark:bg-zinc-500 text-white p-1`}
+        className={`fixed sm:top-0 ${isVisible ? "top-16" : "top-20"}  ${isVisible ? "left-0" : "left-[-25px]"} rounded bg-button-orange dark:bg-zinc-500 text-white p-1`}
       >
         {isVisible ? <GoArrowRight size={20} /> : <GoArrowLeft size={20} />}
       </button>
-      <div className="p-4 flex flex-col items-center">
+      <div className="p-4 flex flex-col items-center mt-16 sm:mt-0">
         <h2 className="text-lg font-bold text-white mb-4"> Recent Items</h2>
         <ul className="text-base text-gray-100 dark:text-gray-300 max-h-[85vh] overflow-y-auto">
           {items.map(item => (

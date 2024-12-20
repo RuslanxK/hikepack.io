947e7db4fdd3f3bdd724d1a825edb8c76ea9e6f9
Ruslan Khomutov <ruslanxkhomutov@gmail.com>
2024-12-20 17:51:32 +0200

    zz

--- client/src/utils/color.ts
+++ client/src/utils/color.ts
@@ -1,11 +1,11 @@
 // src/utils/colorUtils.ts
-export function getRandomDarkColor(existingColors: string[]): string {
+export function getRandomSolidColor(existingColors: string[]): string {
   let color;
   do {
     color = "#";
     for (let i = 0; i < 3; i++) {
-      // Generate a brighter value by limiting the random value closer to 255
-      const part = Math.floor(128 + Math.random() * 128); // 128-255 range
+      // Generate a solid, muted color value in the range of 80-160
+      const part = Math.floor(80 + Math.random() * 80); // 80-160 range
       color += ("0" + part.toString(16)).slice(-2);
     }
   } while (existingColors.includes(color));
